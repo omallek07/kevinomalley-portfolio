91dc8f50036f6f5bd61896437793c6b7b96488a1
Kevin O'Malley <omallek@gmail.com>
2022-03-08 18:42:49 -0600

    adds styles to banner and button

--- components/pages/home/aboutMe.js
+++ components/pages/home/aboutMe.js
@@ -5,9 +5,9 @@ import styles from "./home.module.css";
 const AboutMe = () => {
   const span1 = "Hello everyone! Thank you for visiting my site.";
   const span2 =
-    "A little about me: I am a Fullstack Academy graduate and a professional Javascript developer. I love exploring the world of code and sharpening my skillset with personal projects and online courses.";
+    " A little about me: I am a Fullstack Academy graduate and a professional Javascript developer. I love exploring the world of code and sharpening my skillset with personal projects and online courses.";
   const span3 =
-    "I currently reside in Chicago with my wife and our two dogs, Ollie and Chloe. Please check out my portfolio and feel free to email me to reach out!";
+    " I currently reside in Chicago with my wife and our two dogs, Ollie and Chloe. Please check out my portfolio and feel free to email me to reach out!";
   const spanArray = [span1, span2, span3];
 
   const renderedText = spanArray.map((spanText) => <span>{spanText}</span>);
