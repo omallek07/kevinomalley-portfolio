cdfafd37dcee026378ac7bb3406e0a25a181adc1
Kevin O'Malley <omallek@gmail.com>
2022-03-15 14:52:33 -0500

    adds icons to header

--- components/layout/header/index.js
+++ components/layout/header/index.js
@@ -1,31 +1,48 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./header.module.css";
+import Image from "next/image";
 
 const Header = () => {
   const navLinks = [
     {
       link: "/",
       label: "Home",
+      icon: "/icons/home-icon-white.svg",
     },
     {
       link: "/posts",
-      label: "Blog Posts",
+      label: "Blog",
+      icon: "/icons/blog-icon-white.svg",
     },
     {
       link: "/projects",
       label: "Projects",
+      icon: "/icons/projects-icon-white.svg",
     },
     {
       link: "/portfolio",
       label: "Portfolio",
+      icon: "/icons/resume-icon-white.svg",
     },
   ];
 
   const renderedNavLinks = navLinks.map((navLink) => {
+    const { label, link, icon } = navLink;
     return (
-      <Link key={navLink.label} href={navLink.link}>
-        <a className={styles.navItem}>{navLink.label}</a>
+      <Link key={label} href={link}>
+        <div className={styles.navItem}>
+          <div className={styles.imageWrapper}>
+            <Image
+              alt={label}
+              src={icon}
+              height={30}
+              width={30}
+              layout="responsive"
+            />
+          </div>
+          <a className={styles.navLink}>{label}</a>
+        </div>
       </Link>
     );
   });
